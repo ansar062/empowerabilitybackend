f3ed2c96386eacdd8c177520fb97b2ebc90f96cf
Muhammad Ansar <mmansar000@gmail.com>
2024-05-11 08:54:14 +0500

    multer fix

--- Routes/BlogRoute.js
+++ Routes/BlogRoute.js
@@ -1,7 +1,17 @@
 const { CreateBlog, GetBlogs, GetSingleBlog, PutBlog, DeleteBlog } = require("../Controllers/BlogController");
 const router = require("express").Router();
-const uploadMiddleware = require('../util/multer');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        return cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const uploadMiddleware = multer({storage});
 
 router.post("/createblog", uploadMiddleware.single('file'), CreateBlog);
 router.get("/blogs", GetBlogs);
